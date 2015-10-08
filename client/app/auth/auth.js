18ef177b965d5ca3d51d6a11ae50f243a6bfcd2c
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-10-08 14:34:21 -0700

    (bug) Fixes issue where taken username displayed no error message

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -2,26 +2,29 @@
   'use strict';
 
   angular.module('app')
-  .controller('AuthController', ['$scope', '$window', '$state', 'Auth', 
+  .controller('AuthController', ['$scope', '$window', '$state', 'Auth',
   function ($scope, $window, $state, Auth) {
     $scope.user = {};
+    $scope.signupFail = false;
     //Sign in function and store token into local storage
     //then change state to currentTrip.html
     $scope.signin = function () {
       Auth.signin($scope.user, function (token) {
-          $window.localStorage.setItem('com.vacaypay', token);
-          $state.transitionTo('currentTrip.expense');
-          $scope.signinForm.$setPristine();
+        $window.localStorage.setItem('com.vacaypay', token);
+        $state.transitionTo('currentTrip.expense');
+        $scope.signinForm.$setPristine();
       });
     };
     //Sign up function and store token into local storage
     //then change state to currentTrip.html
     $scope.signup = function () {
       Auth.signup($scope.user, function (token) {
-          $window.localStorage.setItem('com.vacaypay', token);
-          $state.transitionTo('currentTrip.expense');
-          $scope.signupForm.$setPristine();   
+        $scope.signupFail = false;
+        $window.localStorage.setItem('com.vacaypay', token);
+        $state.transitionTo('currentTrip.expense');
+        $scope.signupForm.$setPristine();
       });
+      $scope.signupFail = true;
     };
   }]);
 })();
